1e623b7978bb4005e7f62ca49b5500d2faad202d
Kristoffer Dalby <kradalby@kradalby.no>
2019-05-14 21:22:40 +0200

    adds screenshots

---
+++ screenshots/screenshots.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+pageres --filename='<%= date %>-<%= url %>' --overwrite --delay=5 \
+    https://hugindemo.kradalby.no/ \
+    https://hugindemo.kradalby.no/#/album/content/root/2018/index.json \
+    https://hugindemo.kradalby.no/#/album/content/root/2018/2018-04-22_Biking_to_Lisse/index.json \
+    https://hugindemo.kradalby.no/#/photo/content/root/2018/2018-04-22_Biking_to_Lisse/20180421-122847-IMG_6983.json \
+    https://hugindemo.kradalby.no/#/keyword/content/keywords/Kristoffer_Andreas_Dalby.json \
+    https://hugindemo.kradalby.no/#/keyword/content/keywords/Vestfold.json \
+
