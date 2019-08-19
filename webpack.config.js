a0c5cccef7bd6c28fefc6ac84abf3a84be1fb02e
Kristoffer Dalby <kradalby@kradalby.no>
2019-08-19 17:46:17 +0100

    more build debug

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ const production = mode === "production";
 console.log(mode);
 console.log("Env: ", process.env)
 console.log("MAPBOX: ", process.env.HUGIN_MAPBOX_ACCESS_TOKEN)
+console.log("TESTY: ", process.env.TESTY)
 
 
 
