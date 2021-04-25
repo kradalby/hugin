63382d4fa014a6915c2b33c93150a050a62c12b0
Kristoffer Dalby <kradalby@kradalby.no>
2021-04-25 16:04:09 +0100

    Split up typescript a bit

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import notFound from "url:./images/404.jpg";
 
 console.log(placeholder, loading, notFound);
 
+import initMap from './map';
+
 // FONT AWESOME
 import { library, dom } from "@fortawesome/fontawesome-svg-core";
 import {
@@ -111,130 +113,4 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-// Helper functions
-function resolveElementById(selector: string): Promise<HTMLElement> {
-  let element = document.getElementById(selector);
-  if (element === null) {
-    return new Promise((resolve) => {
-      requestAnimationFrame(resolve);
-    }).then(() => resolveElementById(selector));
-  } else {
-    return Promise.resolve(element);
-  }
-}
 
-// MAPBOX
-require("mapbox-gl/dist/mapbox-gl.css");
-let mapboxgl = require("mapbox-gl");
-mapboxgl.accessToken = process.env.HUGIN_MAPBOX_ACCESS_TOKEN;
-
-let map: mapboxgl.Map | null = null;
-
-// coordinates: [Name : String, [[-80.425, 46.437], [-71.516, 46.437]] : List ( Float, Float ) ]
-function initMap(data: [string, [number, number][]]) {
-  console.log("DEBUG: initMap called with: ", data);
-
-  // ----------------------------------------------
-  // Clean up maps and old maps
-  //
-
-  // Try to force some garbage collection
-  if (map) {
-    map.remove();
-    map = null;
-  }
-
-  // Ugly hack to remove not garbage collected rouge maps
-  document.querySelectorAll("[class^=mapboxgl]").forEach((element) => {
-    if (element.parentNode !== null) {
-      element.parentNode.removeChild(element);
-    }
-  });
-
-  // ----------------------------------------------
-
-  let divName = "map-" + data[0];
-  let coordinates = data[1];
-
-  resolveElementById(divName).then(() => {
-    // Create map
-    map = new mapboxgl.Map({
-      container: divName,
-      style: "mapbox://styles/mapbox/light-v9",
-      zoom: 13,
-      interactive: false,
-      maxZoom: 10,
-      minZoom: 2,
-    });
-
-    let bounds = new mapboxgl.LngLatBounds();
-
-    // Draw markers
-    coordinates.forEach((coordinate) => {
-      new mapboxgl.Marker().setLngLat(coordinate).addTo(map);
-
-      bounds.extend(coordinate);
-    });
-    if (map) {
-      map.fitBounds(bounds, {
-        padding: { top: 65, bottom: 50, left: 50, right: 50 },
-        linear: false,
-        maxZoom: 10,
-      });
-    }
-  });
-}
-
-// FILE DOWNLOAD
-
-// let saveAs = require('./FileSaver.js')
-// require("web-streams-polyfill");
-// let StreamSaver = require("streamsaver");
-//
-// let JSZip = require("jszip");
-// let JSZipUtils = require("jszip-utils");
-//
-// function urlToPromise(url: string) {
-//   return new Promise(function(resolve, reject) {
-//     JSZipUtils.getBinaryContent(url, function(err: Error, data: Uint8Array) {
-//       if (err) {
-//         reject(err);
-//       } else {
-//         resolve(data);
-//       }
-//     });
-//   });
-// }
-//
-// function downloadImages(urls: string[]) {
-//   console.log("[DEBUG] Download images is called with: ", urls);
-//   let zip = new JSZip();
-//   const fileStream = StreamSaver.createWriteStream("download.zip");
-//   const writer = fileStream.getWriter();
-//
-//   urls.forEach(url => {
-//     let filename = url.replace(/.*\//g, "");
-//     console.log(`[DEBUG] Adds file ${filename} to zip`);
-//     zip.file(filename, urlToPromise(url), { binary: true });
-//   });
-//
-//   console.log("[DEBUG] Zip: ", zip);
-//
-//   zip
-//     .generateInternalStream({ type: "uint8array", streamFiles: true })
-//     .on("data", (data: Uint8Array) => {
-//       writer.write(data);
-//       // writer.write(new Blob([data]))
-//     })
-//     .on("end", () => {
-//       console.log("Reached end of zip stream");
-//       writer.close();
-//     })
-//     .on("error", (error: Error) => {
-//       console.log(error);
-//       writer.abort(error);
-//     })
-//     .resume();
-//
-//   return false;
-// }
---
+++ src/map.ts
@@ -0,0 +1,73 @@
+
+// MAPBOX
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = process.env.HUGIN_MAPBOX_ACCESS_TOKEN;
+
+let map: mapboxgl.Map | null = null;
+
+// coordinates: [Name : String, [[-80.425, 46.437], [-71.516, 46.437]] : List ( Float, Float ) ]
+export default function initMap(data: [string, [number, number][]]) {
+  console.log("DEBUG: initMap called with: ", data);
+
+  // ----------------------------------------------
+  // Clean up maps and old maps
+  //
+
+  // Try to force some garbage collection
+  if (map) {
+    map.remove();
+    map = null;
+  }
+
+  // Ugly hack to remove not garbage collected rouge maps
+  document.querySelectorAll("[class^=mapboxgl]").forEach((element) => {
+    if (element.parentNode !== null) {
+      element.parentNode.removeChild(element);
+    }
+  });
+
+  // ----------------------------------------------
+
+  let divName = "map-" + data[0];
+  let coordinates = data[1];
+
+  resolveElementById(divName).then(() => {
+    // Create map
+    map = new mapboxgl.Map({
+      container: divName,
+      style: "mapbox://styles/mapbox/light-v9",
+      zoom: 13,
+      interactive: false,
+      maxZoom: 10,
+      minZoom: 2,
+    });
+
+    let bounds = new mapboxgl.LngLatBounds();
+
+    // Draw markers
+    coordinates.forEach((coordinate) => {
+      new mapboxgl.Marker().setLngLat(coordinate).addTo(map);
+
+      bounds.extend(coordinate);
+    });
+    if (map) {
+      map.fitBounds(bounds, {
+        padding: { top: 65, bottom: 50, left: 50, right: 50 },
+        linear: false,
+        maxZoom: 10,
+      });
+    }
+  });
+}
+
+function resolveElementById(selector: string): Promise<HTMLElement> {
+  let element = document.getElementById(selector);
+  if (element === null) {
+    return new Promise((resolve) => {
+      requestAnimationFrame(resolve);
+    }).then(() => resolveElementById(selector));
+  } else {
+    return Promise.resolve(element);
+  }
+}
