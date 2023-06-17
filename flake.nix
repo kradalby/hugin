4af982a896d09b4a2f9ab99dfa279f2d1dad39f7
Kristoffer Dalby <kristoffer@dalby.cc>
2023-06-17 17:38:26 +0200

    factor out common tsnet logic
    Signed-off-by: Kristoffer Dalby <kristoffer@dalby.cc>

--- flake.nix
+++ flake.nix
@@ -6,231 +6,223 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    , ...
-    }:
-    let
-      huginVersion =
-        if (self ? shortRev)
-        then self.shortRev
-        else "dev";
-    in
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }: let
+    huginVersion =
+      if (self ? shortRev)
+      then self.shortRev
+      else "dev";
+  in
     {
-      overlay = _: prev:
-        let
-          pkgs = nixpkgs.legacyPackages.${prev.system};
-        in
-        rec {
-          huginDeps = pkgs.yarn2nix-moretea.mkYarnPackage {
-            name = "huginYarnDeps";
-            src = pkgs.nix-gitignore.gitignoreSource [ ] ./.;
-          };
-
-          huginElm = pkgs.stdenv.mkDerivation {
-            name = "huginElm";
-            src = pkgs.nix-gitignore.gitignoreSource [ "Makefile" ] ./.;
-
-            buildInputs = with pkgs; [
-              huginDeps
-
-              elmPackages.elm
-              yarn
-              nodejs
-              nodePackages.sass
-              nodePackages.parcel
-
-              python311
-            ];
-
-            postUnpack = ''
-              export HOME="$TMP"
-            '';
-
-            patchPhase = ''
-              rm -rf elm-stuff
-              ln -fs ${huginDeps}/libexec/hugin/node_modules .
-            '';
-
-            configurePhase = pkgs.elmPackages.fetchElmDeps {
-              elmVersion = "0.19.1";
-              elmPackages = import ./elm-srcs.nix;
-              registryDat = ./registry.dat;
-            };
-
-            dontBuild = true;
-
-            installPhase = ''
-              mkdir -p $out
-              parcel build --log-level verbose --dist-dir $out src/index.html
-            '';
-          };
-
-          hugin = pkgs.buildGo120Module {
-            pname = "hugin";
-            version = huginVersion;
-            src = pkgs.nix-gitignore.gitignoreSource [ ] ./.;
-
-            buildInputs = [ huginElm ];
-
-            patchPhase = ''
-              cp -r ${huginElm} dist
-            '';
-
-            vendorSha256 = "sha256-UVRl8g/RunVZka3H9bPM6ifLwLUBL33+IIfdt1fU4jI=";
-          };
+      overlay = _: prev: let
+        pkgs = nixpkgs.legacyPackages.${prev.system};
+      in rec {
+        huginDeps = pkgs.yarn2nix-moretea.mkYarnPackage {
+          name = "huginYarnDeps";
+          src = pkgs.nix-gitignore.gitignoreSource ["Makefile" "go.mod" "go.sum" "*.go"] ./.;
         };
+
+        huginElm = pkgs.stdenv.mkDerivation {
+          name = "huginElm";
+          src = pkgs.nix-gitignore.gitignoreSource ["Makefile" "go.mod" "go.sum" "*.go"] ./.;
+
+          buildInputs = with pkgs; [
+            huginDeps
+
+            elmPackages.elm
+            yarn
+            nodejs
+            nodePackages.sass
+            nodePackages.parcel
+
+            python311
+          ];
+
+          postUnpack = ''
+            export HOME="$TMP"
+          '';
+
+          patchPhase = ''
+            rm -rf elm-stuff
+            ln -fs ${huginDeps}/libexec/hugin/node_modules .
+          '';
+
+          configurePhase = pkgs.elmPackages.fetchElmDeps {
+            elmVersion = "0.19.1";
+            elmPackages = import ./elm-srcs.nix;
+            registryDat = ./registry.dat;
+          };
+
+          dontBuild = true;
+
+          installPhase = ''
+            mkdir -p $out
+            parcel build --log-level verbose --dist-dir $out src/index.html
+          '';
+        };
+
+        hugin = pkgs.buildGo120Module {
+          pname = "hugin";
+          version = huginVersion;
+          src = pkgs.nix-gitignore.gitignoreSource [] ./.;
+
+          buildInputs = [huginElm];
+
+          patchPhase = ''
+            cp -r ${huginElm} dist
+          '';
+
+          vendorSha256 = "sha256-eRelNcv/SI7B8O36aL6KKRpjWoQPNypvasQE/LzaqCo=";
+        };
+      };
     }
     // flake-utils.lib.eachDefaultSystem
-      (system:
-      let
-        pkgs = import nixpkgs {
-          overlays = [ self.overlay ];
-          inherit system;
-        };
-        buildDeps = with pkgs; [
-          elmPackages.elm
-          nodePackages.sass
-          nodePackages.parcel
-          git
-          gnumake
-          go_1_20
-        ];
-        devDeps = with pkgs;
-          buildDeps
-          ++ (with elmPackages; [
-            yarn
-            elm
-            elm-format
-            elm-json
-            elm-analyse
-            elm2nix
-          ]);
-      in
-      rec {
-        # `nix develop`
-        devShell = pkgs.mkShell { buildInputs = devDeps; };
+    (system: let
+      pkgs = import nixpkgs {
+        overlays = [self.overlay];
+        inherit system;
+      };
+      buildDeps = with pkgs; [
+        elmPackages.elm
+        nodePackages.sass
+        nodePackages.parcel
+        git
+        gnumake
+        go_1_20
+      ];
+      devDeps = with pkgs;
+        buildDeps
+        ++ (with elmPackages; [
+          golangci-lint
+          yarn
+          elm
+          elm-format
+          elm-json
+          elm-analyse
+          elm2nix
+        ]);
+    in rec {
+      # `nix develop`
+      devShell = pkgs.mkShell {buildInputs = devDeps;};
 
-        # `nix build`
-        packages = with pkgs; {
-          inherit hugin;
-        };
+      # `nix build`
+      packages = with pkgs; {
+        inherit hugin;
+      };
 
-        defaultPackage = pkgs.hugin;
+      defaultPackage = pkgs.hugin;
 
-        # `nix run`
-        apps.hugin = flake-utils.lib.mkApp {
-          drv = packages.hugin;
-        };
-        defaultApp = apps.hugin;
+      # `nix run`
+      apps.hugin = flake-utils.lib.mkApp {
+        drv = packages.hugin;
+      };
+      defaultApp = apps.hugin;
 
-        overlays.default = self.overlay;
+      overlays.default = self.overlay;
 
-        checks = {
-          format =
-            pkgs.runCommand "check-format"
-              {
-                buildInputs = with pkgs; [
-                  gnumake
-                  nixpkgs-fmt
-                  nodePackages.prettier
-                ];
-              } ''
-              ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt ${./.}
-              ${pkgs.nodePackages.prettier}/bin/prettier --write '**/**.{ts,js,md,yaml,yml,sass,css,scss,html}'
-            '';
-        };
-      })
+      checks = {
+        format =
+          pkgs.runCommand "check-format"
+          {
+            buildInputs = with pkgs; [
+              gnumake
+              nixpkgs-fmt
+              nodePackages.prettier
+            ];
+          } ''
+            ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt ${./.}
+            ${pkgs.nodePackages.prettier}/bin/prettier --write '**/**.{ts,js,md,yaml,yml,sass,css,scss,html}'
+          '';
+      };
+    })
     // {
-      nixosModules.default =
-        { pkgs
-        , lib
-        , config
-        , ...
-        }:
-        let
-          cfg = config.services.hugin;
-        in
-        {
-          options = with lib; {
-            services.hugin = {
-              enable = mkEnableOption "Enable hugin";
+      nixosModules.default = {
+        pkgs,
+        lib,
+        config,
+        ...
+      }: let
+        cfg = config.services.hugin;
+      in {
+        options = with lib; {
+          services.hugin = {
+            enable = mkEnableOption "Enable hugin";
 
-              package = mkOption {
-                type = types.package;
-                description = ''
-                  hugin package to use
-                '';
-                default = pkgs.hugin;
-              };
-
-              dataDir = mkOption {
-                type = types.path;
-                default = "/var/lib/hugin";
-                description = "Path to data dir";
-              };
-
-              user = mkOption {
-                type = types.str;
-                default = "hugin";
-                description = "User account under which hugin runs.";
-              };
-
-              group = mkOption {
-                type = types.str;
-                default = "hugin";
-                description = "Group account under which hugin runs.";
-              };
-
-              tailscaleKeyPath = mkOption {
-                type = types.path;
-              };
-
-              album = mkOption {
-                type = types.path;
-              };
-
-              verbose = mkOption {
-                type = types.bool;
-                default = false;
-              };
-
-              controlUrl = mkOption {
-                type = types.string;
-                default = "";
-              };
+            package = mkOption {
+              type = types.package;
+              description = ''
+                hugin package to use
+              '';
+              default = pkgs.hugin;
             };
-          };
-          config = lib.mkIf cfg.enable {
-            systemd.services.hugin = {
-              enable = true;
-              script =
-                let
-                  args =
-                    [
-                      "--tailscale-auth-key-path ${cfg.tailscaleKeyPath}"
-                      "--album ${cfg.album}"
-                    ]
-                    ++ lib.optionals cfg.verbose [ "--verbose" ];
-                in
-                ''
-                  ${cfg.package}/bin/hugin ${builtins.concatStringsSep " " args}
-                '';
-              wantedBy = [ "multi-user.target" ];
-              after = [ "network-online.target" ];
-              serviceConfig = {
-                User = cfg.user;
-                Group = cfg.group;
-                Restart = "always";
-                RestartSec = "15";
-                WorkingDirectory = "${cfg.dataDir}";
-              };
-              path = [ cfg.package ];
-              environment = { };
+
+            dataDir = mkOption {
+              type = types.path;
+              default = "/var/lib/hugin";
+              description = "Path to data dir";
+            };
+
+            user = mkOption {
+              type = types.str;
+              default = "hugin";
+              description = "User account under which hugin runs.";
+            };
+
+            group = mkOption {
+              type = types.str;
+              default = "hugin";
+              description = "Group account under which hugin runs.";
+            };
+
+            tailscaleKeyPath = mkOption {
+              type = types.path;
+            };
+
+            album = mkOption {
+              type = types.path;
+            };
+
+            verbose = mkOption {
+              type = types.bool;
+              default = false;
+            };
+
+            controlUrl = mkOption {
+              type = types.string;
+              default = "";
             };
           };
         };
+        config = lib.mkIf cfg.enable {
+          systemd.services.hugin = {
+            enable = true;
+            script = let
+              args =
+                [
+                  "--tailscale-auth-key-path ${cfg.tailscaleKeyPath}"
+                  "--album ${cfg.album}"
+                ]
+                ++ lib.optionals cfg.verbose ["--verbose"];
+            in ''
+              ${cfg.package}/bin/hugin ${builtins.concatStringsSep " " args}
+            '';
+            wantedBy = ["multi-user.target"];
+            after = ["network-online.target"];
+            serviceConfig = {
+              User = cfg.user;
+              Group = cfg.group;
+              Restart = "always";
+              RestartSec = "15";
+              WorkingDirectory = "${cfg.dataDir}";
+            };
+            path = [cfg.package];
+            environment = {};
+          };
+        };
+      };
     };
 }
--- main.go
+++ main.go
@@ -6,17 +6,194 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"html"
 	"io/fs"
 	"log"
 	"net"
 	"net/http"
 	"os"
 	"strings"
+	"time"
 
-	"tailscale.com/client/tailscale"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"tailscale.com/tsnet"
 	"tailscale.com/tsweb"
 )
+
+type KraWeb struct {
+	// pubHandlers contains endpoints that should be available over both localhost and Tailscale
+	pubHandlers map[string]http.Handler
+
+	// tsHandlers contains endpoints that should only be available over Tailscale
+	tsHandlers map[string]http.Handler
+
+	// hostname is the name that will be used when joining Tailscale
+	hostname string
+
+	tsKeyPath  string
+	controlURL string
+	verbose    bool
+	dev        string
+	localPort  int
+	logger     *log.Logger
+}
+
+func NewKraWeb(
+	pubHandlers map[string]http.Handler,
+	tsHandlers map[string]http.Handler,
+	hostname string,
+	tsKeyPath string,
+	controlURL string,
+	verbose bool,
+	dev string,
+	localPort int,
+	logger *log.Logger,
+) KraWeb {
+	return KraWeb{
+		pubHandlers: pubHandlers,
+		tsHandlers:  tsHandlers,
+		hostname:    hostname,
+		tsKeyPath:   tsKeyPath,
+		controlURL:  controlURL,
+		verbose:     verbose,
+		dev:         dev,
+		localPort:   localPort,
+		logger:      logger,
+	}
+}
+
+func (k *KraWeb) ListenAndServe() error {
+	mux := http.NewServeMux()
+	tsmux := http.NewServeMux()
+
+	tsweb.Debugger(tsmux)
+
+	k.logger.SetPrefix("kraweb: ")
+	log := k.logger
+
+	tsSrv := &tsnet.Server{
+		Hostname:   k.hostname,
+		Logf:       func(format string, args ...any) {},
+		ControlURL: k.controlURL,
+	}
+
+	if k.tsKeyPath != "" {
+		key, err := os.ReadFile(k.tsKeyPath)
+		if err != nil {
+			return err
+		}
+
+		tsSrv.AuthKey = strings.TrimSuffix(string(key), "\n")
+	}
+
+	if k.verbose {
+		tsSrv.Logf = log.Printf
+	}
+
+	if err := tsSrv.Start(); err != nil {
+		return err
+	}
+
+	localClient, _ := tsSrv.LocalClient()
+
+	tsmux.Handle("/metrics", promhttp.Handler())
+	tsmux.Handle("/who", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		who, err := localClient.WhoIs(r.Context(), r.RemoteAddr)
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+
+			return
+		}
+		fmt.Fprintf(w, "<html><body><h1>Hello, world!</h1>\n")
+		fmt.Fprintf(w, "<p>You are <b>%s</b> from <b>%s</b> (%s)</p>",
+			html.EscapeString(who.UserProfile.LoginName),
+			html.EscapeString(firstLabel(who.Node.ComputedName)),
+			r.RemoteAddr)
+	}))
+
+	for pattern, handler := range k.pubHandlers {
+		mux.Handle(pattern, handler)
+		tsmux.Handle(pattern, handler)
+	}
+
+	for pattern, handler := range k.tsHandlers {
+		tsmux.Handle(pattern, handler)
+	}
+
+	httpSrv := &http.Server{
+		Handler:     mux,
+		ErrorLog:    k.logger,
+		ReadTimeout: 5 * time.Minute,
+	}
+
+	tshttpSrv := &http.Server{
+		Handler:     tsmux,
+		ErrorLog:    k.logger,
+		ReadTimeout: 5 * time.Minute,
+	}
+
+	if *dev != "" {
+		// override default hostname for dev mode
+		if *hostname == defaultHostname {
+			if h, p, err := net.SplitHostPort(*dev); err == nil {
+				if h == "" {
+					h = "localhost"
+				}
+				*hostname = fmt.Sprintf("%s:%s", h, p)
+			}
+		}
+
+		tshttpSrv.Addr = *dev
+
+		log.Printf("Running in dev mode on %s ...", *dev)
+		log.Fatal(tshttpSrv.ListenAndServe())
+	}
+
+	// Starting HTTPS server
+	go func() {
+		ts443, err := tsSrv.Listen("tcp", ":443")
+		if err != nil {
+			log.Printf("failed to start https server: %s", err)
+		}
+		ts443 = tls.NewListener(ts443, &tls.Config{
+			GetCertificate: localClient.GetCertificate,
+		})
+
+		log.Printf("Serving http://%s/ ...", k.hostname)
+		if err := tshttpSrv.Serve(ts443); err != nil {
+			log.Fatalf("failed to start https server in Tailscale: %s", err)
+		}
+	}()
+
+	go func() {
+		ts80, err := tsSrv.Listen("tcp", ":80")
+		if err != nil {
+			log.Printf("failed to start http server: %s", err)
+		}
+
+		log.Printf("Serving http://%s/ ...", k.hostname)
+		if err := tshttpSrv.Serve(ts80); err != nil {
+			log.Fatalf("failed to start http server in Tailscale: %s", err)
+		}
+	}()
+
+	localListen, err := net.Listen("tcp", fmt.Sprintf(":%d", k.localPort))
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Serving http://%s:%d/ ...", "localhost", k.localPort)
+	if err := httpSrv.Serve(localListen); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func firstLabel(s string) string {
+	s, _, _ = strings.Cut(s, ".")
+	return s
+}
 
 const defaultHostname = "hugin"
 
@@ -39,14 +216,14 @@ var (
 	albumDir = flag.String("album", "", "directory containing a Munin album")
 
 	controlURL = flag.String("controlurl", "", "Tailscale Control server, if empty, upstream")
+
+	localPort = flag.Int("port", 56664, "Port to listen to locally")
 )
 
 func main() {
-	err := Run()
-	if err != nil {
+	if err := Run(); err != nil {
 		log.Fatalf("failed to start hugin: %s", err)
 	}
-
 }
 
 //go:embed dist/*
@@ -61,101 +238,38 @@ func distHandler() http.Handler {
 	return http.FileServer(http.FS(sub))
 }
 
-var localClient *tailscale.LocalClient
-
 func Run() error {
 	flag.Parse()
 
-	mux := http.NewServeMux()
-	tsweb.Debugger(mux)
+	logger := log.New(os.Stdout, "hugin: ", log.LstdFlags)
 
-	logger := log.New(os.Stdout, "http: ", log.LstdFlags)
+	handlers := map[string]http.Handler{}
 
-	httpSrv := &http.Server{
-		Handler:  mux,
-		ErrorLog: logger,
-	}
-
-	mux.Handle("/", distHandler())
+	handlers["/"] = distHandler()
 
 	if *albumDir == "" {
-		log.Printf("-album is required to serve an album in production")
+		log.Printf("--album is required to serve an album")
 	} else {
 		log.Printf("Serving content from %s", *albumDir)
-		mux.Handle("/album/", http.StripPrefix("/album", http.FileServer(http.Dir(*albumDir))))
-		mux.Handle("/content/", http.StripPrefix("/content", http.FileServer(http.Dir(*albumDir))))
-	}
-
-	if *dev != "" {
-		// override default hostname for dev mode
-		if *hostname == defaultHostname {
-			if h, p, err := net.SplitHostPort(*dev); err == nil {
-				if h == "" {
-					h = "localhost"
-				}
-				*hostname = fmt.Sprintf("%s:%s", h, p)
-			}
-		}
-
-		httpSrv.Addr = *dev
-
-		log.Printf("Running in dev mode on %s ...", *dev)
-		log.Fatal(httpSrv.ListenAndServe())
+		handlers["/album/"] = http.StripPrefix("/album", http.FileServer(http.Dir(*albumDir)))
+		handlers["/content/"] = http.StripPrefix("/content", http.FileServer(http.Dir(*albumDir)))
 	}
 
 	if *hostname == "" {
 		return errors.New("--hostname, if specified, cannot be empty")
 	}
 
-	srv := &tsnet.Server{
-		Hostname:   *hostname,
-		Logf:       func(format string, args ...any) {},
-		ControlURL: *controlURL,
-	}
+	kraweb := NewKraWeb(
+		handlers,
+		nil,
+		*hostname,
+		*tailscaleKeyPath,
+		*controlURL,
+		*verbose,
+		*dev,
+		*localPort,
+		logger,
+	)
 
-	if *tailscaleKeyPath != "" {
-		key, err := os.ReadFile(*tailscaleKeyPath)
-		if err != nil {
-			log.Fatal("failed to load tailscale auth key")
-		}
-
-		srv.AuthKey = strings.TrimSuffix(string(key), "\n")
-	}
-
-	if *verbose {
-		srv.Logf = log.Printf
-	}
-
-	if err := srv.Start(); err != nil {
-		return err
-	}
-	localClient, _ = srv.LocalClient()
-
-	l80, err := srv.Listen("tcp", ":80")
-	if err != nil {
-		return err
-	}
-
-	// Starting HTTPS server
-	go func() {
-		l443, err := srv.Listen("tcp", ":443")
-		if err != nil {
-			log.Printf("failed to start https server: %s", err)
-		}
-		l443 = tls.NewListener(l443, &tls.Config{
-			GetCertificate: localClient.GetCertificate,
-		})
-
-		log.Printf("Serving http://%s/ ...", *hostname)
-		if err := httpSrv.Serve(l443); err != nil {
-			log.Printf("failed to start https server: %s", err)
-		}
-	}()
-
-	// TODO: Add support for magic auto HTTPS
-	log.Printf("Serving http://%s/ ...", *hostname)
-	if err := httpSrv.Serve(l80); err != nil {
-		return err
-	}
-	return nil
+	return kraweb.ListenAndServe()
 }
