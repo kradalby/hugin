fdb48141ed28284d5c9c56ef7b3f7bf4381f6820
Kristoffer Dalby <kristoffer@dalby.cc>
2024-07-05 22:50:33 +0200

    logging
    Signed-off-by: Kristoffer Dalby <kristoffer@dalby.cc>

--- main.go
+++ main.go
@@ -69,10 +69,10 @@ func distHandler() http.Handler {
 	return http.FileServer(http.FS(sub))
 }
 
-func loggingFileServer(urlPath, path string) http.Handler {
+func loggingHandler(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s %s", r.Method, r.URL.Path)
-		http.StripPrefix(urlPath, http.FileServer(http.Dir(path))).ServeHTTP(w, r)
+		log.Println(r.Method, r.URL.Path)
+		h.ServeHTTP(w, r)
 	})
 }
 
@@ -98,8 +98,8 @@ func Run() error {
 		log.Printf("--album is required to serve an album")
 	} else {
 		log.Printf("Serving content from %s", *albumDir)
-		k.Handle("/album/", loggingFileServer("/album", *albumDir))
-		k.Handle("/content/", loggingFileServer("/content", *albumDir))
+		k.Handle("/album/", loggingHandler(http.FileServer(http.Dir(*albumDir))))
+		k.Handle("/content/", loggingHandler(http.FileServer(http.Dir(*albumDir))))
 	}
 
 	if *hostname == "" {
