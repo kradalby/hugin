4c7fbdc5cf22b8a5543197a95dfc728daa26c285
Kristoffer Dalby <kristoffer@dalby.cc>
2026-08-04 14:44:11 +0000

    build: regenerate elm dependency pins
    elm/html 1.0.1, elm/json 1.1.4, elm/virtual-dom 1.0.5. registry.dat is
    now scoped to the dependency closure, not the full package index.

--- elm-srcs.nix
+++ elm-srcs.nix
@@ -1,4 +1,5 @@
 {
+
   "NoRedInk/elm-json-decode-pipeline" = {
     sha256 = "1k241pjz1wj5rqv95f1j86msa0s0p6w4v8n0jn26aw6cbadw363b";
     version = "1.0.1";
@@ -35,8 +36,8 @@
   };
 
   "elm/html" = {
-    sha256 = "1n3gpzmpqqdsldys4ipgyl1zacn0kbpc3g4v3hdpiyfjlgh8bf3k";
-    version = "1.0.0";
+    sha256 = "0ljwld3yk682qimkwdsbf4vb3fsv3y2hlisym94i705gx18dz6l8";
+    version = "1.0.1";
   };
 
   "elm/http" = {
@@ -45,8 +46,8 @@
   };
 
   "elm/json" = {
-    sha256 = "0kjwrz195z84kwywaxhhlnpl3p251qlbm5iz6byd6jky2crmyqyh";
-    version = "1.1.3";
+    sha256 = "0w1n61m2b9zbwp1yx8zhvir7gqy66mkm22rpq8hmnvgkcwnhxalb";
+    version = "1.1.4";
   };
 
   "elm/random" = {
@@ -85,7 +86,7 @@
   };
 
   "elm/virtual-dom" = {
-    sha256 = "1yvb8px2z62xd578ag2q0r5hd1vkz9y7dfkx05355iiy1d7jwq4v";
-    version = "1.0.3";
+    sha256 = "0zc3dfzqsma8lrkbm6nkjqbsfcv6h5nja1rh7ybkinw1jk0kfdv6";
+    version = "1.0.5";
   };
 }
